759ccecfe34b46c3c8d03ee903a9ead9172bbb66
Eric Malm <emalm@pivotal.io>
2017-09-22 17:55:21 -0700

    Deployer can opt into adding cell-IP tag
    [#151345457]

--- main.go
+++ main.go
@@ -20,7 +20,13 @@ func main() {
 
 	datadogApiKey := os.Getenv("DATADOG_API_KEY")
 	deploymentName := os.Getenv("DEPLOYMENT_NAME")
-	go postHeartbeat(appIndex, datadogApiKey, deploymentName)
+
+	cellIP := ""
+	if os.Getenv("INCLUDE_CELL_IP_TAG") == "true" {
+		cellIP = os.Getenv("CF_INSTANCE_IP")
+	}
+
+	go postHeartbeat(appIndex, cellIP, datadogApiKey, deploymentName)
 
 	err = http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 	if err != nil {
@@ -51,13 +57,16 @@ type DatadogMetric struct {
 
 type Point [2]int
 
-func postHeartbeat(appIndex int, datadogApiKey string, deploymentName string) {
+func postHeartbeat(appIndex int, cellIP, datadogApiKey, deploymentName string) {
 	url := "https://app.datadoghq.com/api/v1/series?api_key=" + datadogApiKey
 
 	client := http.DefaultClient
 	tags := []string{
 		"deployment:" + deploymentName,
 		fmt.Sprintf("diego-canary-app:%d", appIndex),
+	}
+	if cellIP != "" {
+		tags = append(tags, "cell-ip:"+cellIP)
 	}
 
 	for {
