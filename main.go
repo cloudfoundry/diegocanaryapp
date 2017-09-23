68d0a6ccaa37d3b5e5733d2544cb1cbb22db25d1
Eric Malm <emalm@pivotal.io>
2017-09-22 17:47:13 -0700

    Marshal JSON for Datadog payload
    [#151345457]

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
@@ -38,29 +39,48 @@ func helloFromInstance(index int) http.Handler {
 	})
 }
 
+type DatadogSeries struct {
+	Series []DatadogMetric `json:"series"`
+}
+
+type DatadogMetric struct {
+	Metric string   `json:"metric"`
+	Points []Point  `json:"points"`
+	Tags   []string `json:"tags"`
+}
+
+type Point [2]int
+
 func postHeartbeat(appIndex int, datadogApiKey string, deploymentName string) {
-	url := fmt.Sprintf(
-		"https://app.datadoghq.com/api/v1/series?api_key=%s",
-		datadogApiKey,
-	)
+	url := "https://app.datadoghq.com/api/v1/series?api_key=" + datadogApiKey
 
 	client := http.DefaultClient
+	tags := []string{
+		"deployment:" + deploymentName,
+		fmt.Sprintf("diego-canary-app:%d", appIndex),
+	}
 
 	for {
 		time.Sleep(5 * time.Second)
 
-		jsonString := []byte(fmt.Sprintf(`{"series":`+
-			`[{`+
-			`"metric":"diego.canary.app.instance",`+
-			`"points":[[%d, 1]],`+
-			`"tags":["deployment:%s", "diego-canary-app:%d"]`+
-			`}]`+
-			`}`,
-			time.Now().Unix(),
-			deploymentName,
-			appIndex,
-		))
-		req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonString))
+		series := DatadogSeries{
+			Series: []DatadogMetric{{
+				Metric: "diego.canary.app.instance",
+				Tags:   tags,
+				Points: []Point{{
+					int(time.Now().Unix()),
+					1,
+				}},
+			}},
+		}
+		jsonPayload, err := json.Marshal(series)
+		if err != nil {
+			println(err.Error())
+			continue
+		}
+		fmt.Println(string(jsonPayload))
+
+		req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonPayload))
 		if err != nil {
 			println(err.Error())
 		}
